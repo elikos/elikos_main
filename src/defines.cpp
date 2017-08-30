838f67408d581c26bff9293c5d48147e8d5d1f66
Elikos <terriault.eva@gmail.com>
2017-08-30 16:52:03 -0400

    Clean up and renaming of elikos_ros package to elikos_main.

---
+++ src/defines.cpp
@@ -0,0 +1,132 @@
+/*
+ * defines.cpp
+ *
+ *  Created on: Jan 15, 2015
+ *      Author: Myriam Claveau-Mallet
+ *      Brief: This file contains all topics definitions and functions to create publishers
+ *             for these topics, and other static or const variables and enum. Also are
+ *             included commun includes and pre-compiler defines.
+ */
+
+
+#ifndef _DEFINES_CPP_
+#define _DEFINES_CPP_
+
+
+/* *************************************************************************************************
+ * ***           INCLUDES
+ * *************************************************************************************************
+ */
+
+
+#include <ros/ros.h>
+#include <vector>
+#include <string>
+
+
+/* *************************************************************************************************
+ * ***           DEFINES
+ * *************************************************************************************************
+ */
+
+#ifndef PI
+#define PI 3.14159265
+#endif
+
+
+/* *************************************************************************************************
+ * ***           ENUMS
+ * *************************************************************************************************
+ */
+
+
+/*
+ * VERY IMPORTANT NOTE REGARDING TOPICS
+ *
+ * For every new topic included in the following lists, one should ABSOLUTLY
+ *
+ *     (1) include new topic in the enum,
+ *     (2) include new topic string in the std::vector TOPICS_NAMES,
+ *     (3) make sure both enum and vector entry have the SAME POSITION.
+ *
+ * This is because each enum value represents an index in TOPICS_NAMES vector.
+ */
+
+///
+/// Define topics to avoid confusion in topics denomination
+///
+enum topics
+{
+    robotsPos = 0,
+    robotsim_robot_markers,
+    robotsim_mav_marker,
+    robotsim_setpoint_marker,
+    robotsim_arena_marker,
+    camera_image_raw,
+    mavros_setpoint_local_position,
+    mavros_position_local, // quad position
+    mavros_imu_data, // quad orientation
+    mavros_rc_in,
+    elikos_robotdetect_altitude,
+    NB_TOPICS
+};
+
+
+///
+/// Define robots possible types. Will be used to identify robots types
+/// in the message RobotPos.
+///
+enum robotTypes
+{
+	groundRobot = 0,
+	obstacleRobot,
+	quadRobot,
+	unknown
+};
+
+typedef robotTypes RobotType;
+
+
+enum Color
+{
+    RED = 0, GREEN
+};
+
+
+/* *************************************************************************************************
+ * ***           STATIC VARIABLES
+ * *************************************************************************************************
+ */
+
+///
+/// Provide access to enum names
+///
+const std::string arr[] = // TOPICS_NAMES
+{
+	"robotDetect/robotsPos", // robotDetec topic : communicate the located robots positions
+	"robotsim/robot_markers",
+	"robotsim/mav_marker",
+	"robotsim/setpoint_marker",
+	"robotsim/arena_marker",
+    "camera/image_raw",
+	"mavros/setpoint/local_position", // quad : set position
+	"mavros/position/local", // quad position
+	"mavros/imu/data", // quad orientation
+    "mavros/rc/in",
+    "elikos_robotdetect/altitude"
+
+};
+static const std::vector<std::string> TOPIC_NAMES
+(
+    arr, arr + sizeof(arr) / sizeof(arr[0])
+);
+
+
+///
+/// Camera size
+///
+static const int CAM_HEIGHT = 480;
+static const int CAM_WIDTH = 640;
+
+
+#endif // _DEFINES_CPP_
---
+++ src/elikos_origin_init/elikos_origin_init.cpp
@@ -0,0 +1,106 @@
+#include <ros/ros.h>
+#include <tf/transform_listener.h>
+#include <tf/transform_broadcaster.h>
+#include <tf/transform_datatypes.h>
+#include <std_srvs/Empty.h>
+#include <sensor_msgs/Imu.h>
+#include <string>
+
+const std::string ELIKOS_ARENA_ORIGIN = "elikos_arena_origin";
+const std::string ELIKOS_LOCAL_ORIGIN = "elikos_local_origin";
+const std::string ELIKOS_FCU = "elikos_fcu";
+const std::string ELIKOS_VISION = "elikos_vision";
+const std::string ELIKOS_ATTITUDE = "elikos_attitude";
+
+bool isInit_ = false;
+bool lookupDone = false;
+bool initialize(std_srvs::Empty::Request  &req,
+         std_srvs::Empty::Response &res)
+{
+  isInit_ = true;
+  lookupDone = false;
+  return true;
+}
+
+tf::Quaternion attitude_;
+tf::Quaternion attitude_offset_;
+
+void imu_callback(const sensor_msgs::Imu::ConstPtr& input)
+{
+	tf::quaternionMsgToTF(input->orientation, attitude_);
+}
+
+int main(int argc, char* argv[])
+{
+  ros::init( argc, argv, "elikos_origin_init" );
+
+  ros::NodeHandle n;
+
+  tf::TransformListener tf_listener_;
+  tf::TransformBroadcaster tf_broadcaster_;
+  tf::StampedTransform arenaOriginTransform;
+  ros::Subscriber sub_;
+  sub_ = n.subscribe("/vn100/imu/imu", 1, imu_callback);
+
+
+  while (!tf_listener_.canTransform(ELIKOS_LOCAL_ORIGIN, ELIKOS_FCU, ros::Time(0))) 
+  {
+      tf_broadcaster_.sendTransform(tf::StampedTransform(tf::Transform(tf::Quaternion::getIdentity(), tf::Vector3(0.0, 0.0, 0.17)), 
+          ros::Time::now(), ELIKOS_LOCAL_ORIGIN, ELIKOS_VISION));
+  }
+
+  tf::StampedTransform initialFcu;
+  try {
+    tf_listener_.waitForTransform(ELIKOS_LOCAL_ORIGIN, ELIKOS_FCU, ros::Time::now(), ros::Duration(5.0));
+    tf_listener_.lookupTransform(ELIKOS_LOCAL_ORIGIN, ELIKOS_FCU, ros::Time(0), initialFcu);
+  }
+  catch (tf::TransformException &ex) {
+    ROS_ERROR("Origin init failed!!!! Exception : %s",ex.what());
+  }
+
+
+  ros::ServiceServer service = n.advertiseService("elikos_origin_init", initialize);
+  
+
+  ros::Rate r(10);
+  while(ros::ok())
+  {
+    if(isInit_)
+    {
+      if(!lookupDone)
+      {
+        try {
+          // Prevent console spam.
+          tf_listener_.waitForTransform(ELIKOS_LOCAL_ORIGIN, ELIKOS_FCU, ros::Time(0), ros::Duration(1.0));
+    			tf_listener_.lookupTransform(ELIKOS_LOCAL_ORIGIN, ELIKOS_FCU, ros::Time(0), arenaOriginTransform);
+          tf::Vector3 origin = initialFcu.getOrigin();
+          origin.setZ(0);
+          tf::Quaternion rotation = initialFcu.getRotation();
+          double yaw = tf::getYaw(rotation);
+          rotation.setRPY(0,0,yaw);
+          ROS_INFO_STREAM("Initialisation : Yaw diff is : "<<yaw);
+          arenaOriginTransform.setOrigin(origin);
+          arenaOriginTransform.setRotation(rotation);
+          lookupDone = true;
+          attitude_offset_ = attitude_;
+    		}
+    		catch (tf::TransformException &ex) {
+    			ROS_ERROR("Origin init failed!!!! Exception : %s",ex.what());
+          tf_broadcaster_.sendTransform(tf::StampedTransform(tf::Transform(tf::Quaternion::getIdentity(), tf::Vector3(0.0, 0.0, 0.17)), 
+              ros::Time::now(), ELIKOS_ARENA_ORIGIN, ELIKOS_VISION));
+    		}
+      }
+      tf_broadcaster_.sendTransform(tf::StampedTransform(arenaOriginTransform.inverse(), ros::Time::now(), ELIKOS_ARENA_ORIGIN, ELIKOS_LOCAL_ORIGIN));
+      tf_broadcaster_.sendTransform(tf::StampedTransform(tf::Transform(attitude_offset_.inverse() * attitude_, tf::Vector3(0,0,0)), ros::Time::now(), ELIKOS_ARENA_ORIGIN, ELIKOS_ATTITUDE));
+    }
+    else
+    {
+      tf_broadcaster_.sendTransform(tf::StampedTransform(tf::Transform::getIdentity(), ros::Time::now(), ELIKOS_ARENA_ORIGIN, ELIKOS_LOCAL_ORIGIN));
+    }
+
+    ros::spinOnce();
+    r.sleep();
+  }
+
+  return 0;
+}
